2b81870232630d428e3f40b5b68d159a37f2d672
Hiigaran <Graham.Pedersen@gmail.com>
2013-10-28 23:24:27 +0000

    sorry sarah =(

--- desugar.rkt
+++ desugar.rkt
@@ -1,13 +1,24 @@
+; This is a very simple desugarer for a simple scheme like language based on the article at http://matt.might.net/articles/desugaring-scheme/ over time we will grow this into a consideral subset of the racket language which we will grow out of this file by adding and changing functionality
+
 ; TODO remove language dependances from racket
+
+
 #lang racket
 
-;move to library this is not the right place
-(define gen-var 
-    ((lambda (init)
-        (define x init)
-        (lambda (sym) 
-            (set! x (+ 1 x))
-            `(,sym x)))0))
+
+; Entry Point
+
+(define (desugar-input)
+    (define program (car (read-input)))
+
+    ; convert all the top leves forms into defines
+    (set! program (tops-to-defs program))
+    ; now that everthing is defines we can desugar defines
+    (set! program (map desugar-define program))
+    (displayln (pretty-format program)))
+
+
+; Helper Functions
 
 ; reads all input from std in into a list
 (define (read-input)
@@ -16,56 +27,100 @@
         '()
         (cons line (read-input))))
 
-(define (desugar-input)
-    (define program (car (read-input)))
-    (displayln '(this is the input program:))
-    (displayln (pretty-format input))
-    (displayln '(output program:))
-    ;convert all the top leves into defines
-    (set! program (tops-to-defs input))
-    ;now that everthing is defines we can do this
-    (set! program (map desugar-define program))
-    (displayln (pretty-format program)))
 
-;desugar all our define statments
-(define (desugar-define def) 
-    (cond
-        []
-        [else `(cannot desugar define, def)
-
-(define (desugar-define def) def)
-(define (desugar-exp exp) exp)
-(define (desugar-body body) body)
-(define (desugar-quote s-exp) s-exp)
-(define (desugar-quasi-quote s-exp) s-exp)
+; Desugaring Functions
 
 
-;converts all top level forms into defines
 (define (tops-to-defs tops)
     (define (top-to-def top)
         (displayln top)
         (cond 
-            [(function-def? top) `(define ,(caadr top) (lambda ,(cdadr top) . ,(caddr top)))]
+            [(function-def? top) (function-def->var-def top)]
             [(var-def? top) top]
-            [else `(define ,(gensym '_) ,(cdr top))]))
+            [else `(define ,(gensym '_) ,top)]))
 
     (map top-to-def tops))
 
 
+; desugar define statements of the form (define ,v ,exp)
+(define (desugar-define def)
+    (cond
+        [(var-def? def) `(define ,(cadr def) ,(desugar-exp (caddr def)))]
+        [else `(cannot desugar define ,def)]))
+
+(define (desugar-exp exp)
+    (cond
+        [(symbol? exp) exp]
+        [(atomic? exp) exp]
+        [(quote?  exp) (desugar-quote exp)]
+        [(let?    exp   ) (desugar-let exp)]
+        [else (displayln `(could not desugar expression ,exp))]))
+    
+(define (desugar-body body) body)
+(define (desugar-quote s-exp) s-exp)
+(define (desugar-quasi-quote s-exp) s-exp)
+
+(define (desugar-let exp)
+    (define vars (cadr exp))
+    (define body (caddr exp))
+
+        (define var-getter 
+            (lambda (x)
+                (if (null? x)
+                    '()
+                    (cons (caar x) (var-getter (cdr x))))))
+    (define exp-getter 
+            (lambda (x)
+                (if (null? x)
+                    '()
+                    (cons (cadar x) (exp-getter (cdr x))))))
+
+    (define var-names (var-getter vars))
+    (define exps (exp-getter vars))
+    `((lambda ,var-names ,(desugar-body body))
+        ,@(map desugar-exp exps)))
+
+                            
+(define (function-def->var-def def)
+    `(define ,(caadr def) (lambda ,(cdadr def) ,(caddr def))))
+
 ; ----- matching helper functions -----
 
 
-;matches `(define (,f ,params ...) . ,body)
+; matches `(define (,f ,params ...) . ,body)
 (define (function-def? top)
-    (if (and (eq? 'define (car top)) (list? (cadr top)))
-        #t
-        #f))
+    (and (eq? 'define (car top)) (list? (cadr top))))
 
 ; matches `(define ,v ,exp)
-(define (var-def? top)
-    (if (and (eq? 'define (car top)) (not (list? (cadr top))))
-        #t
-        #f))
+(define (var-def? exp)
+    (and (eq? 'define (car exp)) (not (list? (cadr exp)))))
+
+; matchs any quoted list
+(define (quote? exp)
+    (cond
+        [(eq? 'quote (car exp)) #t]
+        [else #f]))
+
+(define (let? exp)
+    (cond
+        [(and (eq? 'let (car exp)) (list? (cadr exp)))  #t]
+        [else #f]))
+
+(define (letrec? exp)
+    (cond
+        [(and (eq? 'letrec (car exp)) (list? (cadr exp)))  #t]
+        [else #f]))
+
+; matches any atomic
+(define (atomic? exp)
+  (cond
+    [(number? exp)   #t]
+    [(string? exp)   #t]
+    [(boolean? exp)  #t]
+    [(eq? 'lambda (car exp))  #t]
+    [(eq? 'quote (car exp)) #t]
+    [(eq? 'void (car exp))       #t]
+    [else          #f]))
 
 
 (desugar-input)
