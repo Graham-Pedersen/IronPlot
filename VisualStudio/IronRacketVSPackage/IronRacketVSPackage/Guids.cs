fa143fddbaa3e2b21ed1f9e632fd47680630c3bc
Scott Bauer <sbauer@eng.utah.edu>
2013-11-17 15:30:53 -0700

    added visual studio package

---
+++ VisualStudio/IronRacketVSPackage/IronRacketVSPackage/Guids.cs
@@ -0,0 +1,16 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace IronPlot.IronRacketVSPackage
+{
+    static class GuidList
+    {
+        public const string guidIronRacketVSPackagePkgString = "edee9ac0-f922-4851-a6c7-d6bafa5e4388";
+        public const string guidIronRacketVSPackageCmdSetString = "5c48336f-bd54-4e9a-b1bc-1e59ae4df9b7";
+        public const string guidIronRacketVSPackageEditorFactoryString = "a37cef70-1331-4274-a119-79d407ee7743";
+
+        public static readonly Guid guidIronRacketVSPackageCmdSet = new Guid(guidIronRacketVSPackageCmdSetString);
+        public static readonly Guid guidIronRacketVSPackageEditorFactory = new Guid(guidIronRacketVSPackageEditorFactoryString);
+    };
+}
---
+++ VisualStudio/IronRacketVSPackage/IronRacketVSPackage/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("IronRacketVSPackage")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("IronPlot")]
+[assembly: AssemblyProduct("IronRacketVSPackage")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]   
+[assembly: ComVisible(false)]     
+[assembly: CLSCompliant(false)]
+[assembly: NeutralResourcesLanguage("en-US")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: InternalsVisibleTo("IronRacketVSPackage_IntegrationTests, PublicKey=002400000480000094000000060200000024000052534131000400000100010005cddf40a1273e0b119dfc09542c5554dca30b843581f360a34ec2036e0dd9cafbf350e36a6c305b7c4e731d2bf8c99eb3ebdaccfc04828c40f099c7f042945d0282c5b1e1448293fa41c384f7ed93957a9b2a8ef4a0d124c7d7554ca0dd833d53a8c4d4c671b79022d030be7d3405606c3e81cc23c27a774fa8ea4204734ca9")]
+[assembly: InternalsVisibleTo("IronRacketVSPackage_UnitTests, PublicKey=002400000480000094000000060200000024000052534131000400000100010005cddf40a1273e0b119dfc09542c5554dca30b843581f360a34ec2036e0dd9cafbf350e36a6c305b7c4e731d2bf8c99eb3ebdaccfc04828c40f099c7f042945d0282c5b1e1448293fa41c384f7ed93957a9b2a8ef4a0d124c7d7554ca0dd833d53a8c4d4c671b79022d030be7d3405606c3e81cc23c27a774fa8ea4204734ca9")]
