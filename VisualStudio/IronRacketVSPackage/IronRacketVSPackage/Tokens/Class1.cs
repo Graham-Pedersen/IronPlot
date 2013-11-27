667d73fd97474bf82bc5d72a1101323d10f863ec
athendrix <andrewthendrix@gmail.com>
2013-11-27 10:30:13 -0700

    Added test start point for Tokens
    Added test start point for Tokens and also added references that we'll
    probably need pulling ideas from ook source.
    I'm not sure if this will work, but we have to start somewhere, and we
    can always revert.

---
+++ VisualStudio/IronRacketVSPackage/IronRacketVSPackage/Tokens/Class1.cs
@@ -0,0 +1,11 @@
+﻿namespace IronPlot.IronRacketVSPackage
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.Composition;
+    using Microsoft.VisualStudio.Text;
+    using Microsoft.VisualStudio.Text.Classification;
+    using Microsoft.VisualStudio.Text.Editor;
+    using Microsoft.VisualStudio.Text.Tagging;
+    using Microsoft.VisualStudio.Utilities;
+}
