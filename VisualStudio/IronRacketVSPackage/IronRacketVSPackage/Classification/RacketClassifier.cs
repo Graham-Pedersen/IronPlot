7cc5b4d2e7879f6d57c1ce1997ae5da1d61f1ab8
athendrix <andrewthendrix@gmail.com>
2013-12-02 23:56:56 -0700

    Still doesn't highlight like I expect...
    However, if go to file, new, file, you can choose to make a new racket
    file! :)

---
+++ VisualStudio/IronRacketVSPackage/IronRacketVSPackage/Classification/RacketClassifier.cs
@@ -0,0 +1,83 @@
+﻿
+
+namespace IronPlot.IronRacketVSPackage
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.Composition;
+    using Microsoft.VisualStudio.Text;
+    using Microsoft.VisualStudio.Text.Classification;
+    using Microsoft.VisualStudio.Text.Editor;
+    using Microsoft.VisualStudio.Text.Tagging;
+    using Microsoft.VisualStudio.Utilities;
+
+
+    [Export(typeof(ITaggerProvider))]
+    [ContentType("racket")]
+    [TagType(typeof(ClassificationTag))]
+    internal sealed class RacketClassifierProvider : ITaggerProvider
+    {
+
+        [Export]
+        [Name("racket")]
+        [BaseDefinition("code")]
+        internal static ContentTypeDefinition RacketContentType = null;
+
+        [Export]
+        [FileExtension(".plot")]
+        [ContentType("racket")]
+        internal static FileExtensionToContentTypeDefinition RacketFileType = null;
+
+        [Import]
+        internal IClassificationTypeRegistryService ClassificationTypeRegistry = null;
+
+        [Import]
+        internal IBufferTagAggregatorFactoryService aggregatorFactory = null;
+
+        public ITagger<T> CreateTagger<T>(ITextBuffer buffer) where T : ITag
+        {
+
+            ITagAggregator<RacketTokenTag> racketTagAggregator =
+                                            aggregatorFactory.CreateTagAggregator<RacketTokenTag>(buffer);
+
+            return new RacketClassifier(buffer, racketTagAggregator, ClassificationTypeRegistry) as ITagger<T>;
+        }
+    }
+
+    internal sealed class RacketClassifier : ITagger<ClassificationTag>
+    {
+        ITextBuffer _buffer;
+        ITagAggregator<RacketTokenTag> _aggregator;
+        IDictionary<RacketTokenTypes, IClassificationType> _racketTypes;
+
+        internal RacketClassifier(ITextBuffer buffer,
+                               ITagAggregator<RacketTokenTag> racketTagAggregator,
+                               IClassificationTypeRegistryService typeService)
+        {
+            _buffer = buffer;
+            _aggregator = racketTagAggregator;
+            _racketTypes = new Dictionary<RacketTokenTypes, IClassificationType>();
+            _racketTypes[RacketTokenTypes.Define] = typeService.GetClassificationType("define");
+            _racketTypes[RacketTokenTypes.Cdr] = typeService.GetClassificationType("cdr");
+            _racketTypes[RacketTokenTypes.Car] = typeService.GetClassificationType("car");
+        }
+
+        public event EventHandler<SnapshotSpanEventArgs> TagsChanged
+        {
+            add { }
+            remove { }
+        }
+
+        public IEnumerable<ITagSpan<ClassificationTag>> GetTags(NormalizedSnapshotSpanCollection spans)
+        {
+
+            foreach (var tagSpan in this._aggregator.GetTags(spans))
+            {
+                var tagSpans = tagSpan.Span.GetSpans(spans[0].Snapshot);
+                yield return
+                    new TagSpan<ClassificationTag>(tagSpans[0],
+                                                   new ClassificationTag(_racketTypes[tagSpan.Tag.type]));
+            }
+        }
+    }
+}
---
+++ VisualStudio/IronRacketVSPackage/IronRacketVSPackage/Tokens/RacketTokenTypes.cs
@@ -0,0 +1,86 @@
+﻿namespace IronPlot.IronRacketVSPackage
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.Composition;
+    using Microsoft.VisualStudio.Text;
+    using Microsoft.VisualStudio.Text.Classification;
+    using Microsoft.VisualStudio.Text.Editor;
+    using Microsoft.VisualStudio.Text.Tagging;
+    using Microsoft.VisualStudio.Utilities;
+
+    public enum RacketTokenTypes
+    {
+        Define, Cdr, Car
+    }
+
+    [Export(typeof(ITaggerProvider))]
+    [ContentType("racket")]
+    [TagType(typeof(RacketTokenTag))]
+    internal sealed class RacketTokenTagProvider : ITaggerProvider
+    {
+
+        public ITagger<T> CreateTagger<T>(ITextBuffer buffer) where T : ITag
+        {
+            return new RacketTokenTagger(buffer) as ITagger<T>;
+        }
+    }
+
+    public class RacketTokenTag : ITag
+    {
+        public RacketTokenTypes type { get; private set; }
+
+        public RacketTokenTag(RacketTokenTypes type)
+        {
+            this.type = type;
+        }
+    }
+
+    internal sealed class RacketTokenTagger : ITagger<RacketTokenTag>
+    {
+
+        ITextBuffer _buffer;
+        IDictionary<string, RacketTokenTypes> _racketTypes;
+
+        internal RacketTokenTagger(ITextBuffer buffer)
+        {
+            _buffer = buffer;
+            _racketTypes = new Dictionary<string, RacketTokenTypes>();
+            _racketTypes["define"] = RacketTokenTypes.Define;
+            _racketTypes["cdr"] = RacketTokenTypes.Cdr;
+            _racketTypes["car"] = RacketTokenTypes.Car;
+        }
+
+        public event EventHandler<SnapshotSpanEventArgs> TagsChanged
+        {
+            add { }
+            remove { }
+        }
+
+        public IEnumerable<ITagSpan<RacketTokenTag>> GetTags(NormalizedSnapshotSpanCollection spans)
+        {
+
+            foreach (SnapshotSpan curSpan in spans)
+            {
+                ITextSnapshotLine containingLine = curSpan.Start.GetContainingLine();
+                int curLoc = containingLine.Start.Position;
+                string[] tokens = containingLine.GetText().ToLower().Split(' ');
+
+                foreach (string racketToken in tokens)
+                {
+                    if (_racketTypes.ContainsKey(racketToken))
+                    {
+                        var tokenSpan = new SnapshotSpan(curSpan.Snapshot, new Span(curLoc, racketToken.Length));
+                        if (tokenSpan.IntersectsWith(curSpan))
+                            yield return new TagSpan<RacketTokenTag>(tokenSpan,
+                                                                  new RacketTokenTag(_racketTypes[racketToken]));
+                    }
+
+                    //add an extra char location because of the space
+                    curLoc += racketToken.Length + 1;
+                }
+            }
+
+        }
+    }
+}
